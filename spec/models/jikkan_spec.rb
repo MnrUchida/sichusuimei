88cdb3d4529d5226087903e9d42bb6dd1ccd2bec
MnrUchida <mukenan108@gmail.com>
2013-06-21 17:55:29 +0900

    天財を追加

--- spec/models/jikkan_spec.rb
+++ spec/models/jikkan_spec.rb
@@ -744,3 +744,59 @@ describe Jikkan, "学士" do
     it_behaves_like :gakushi_get_validation, pattern
   end
 end
+
+describe Jikkan, "学士" do
+  shared_examples_for :gakushi_check_validation do |params|
+    subject{Jikkan.by_key(key).gakushi?(relation)}
+    it_behaves_like :relation_check_validation_junishi, params
+  end
+
+  shared_examples_for :gakushi_get_validation do |params|
+    subject{Jikkan.by_key(key).gakushi}
+    it_behaves_like :relation_get_validation_junishi, params
+  end
+
+  test_patterns = [{:key => 'kinoe', :relation_value => 'ne', :expected_value => true},
+                   {:key => 'kinoto', :relation_value => 'i', :expected_value => true},
+                   {:key => 'hinoe', :relation_value => 'u', :expected_value => true},
+                   {:key => 'hinoto', :relation_value => 'tora', :expected_value => true},
+                   {:key => 'tsuchinoe', :relation_value => 'uma', :expected_value => true},
+                   {:key => 'tsuchinoto', :relation_value => 'mi', :expected_value => true},
+                   {:key => 'kanoe', :relation_value => 'uma', :expected_value => true},
+                   {:key => 'kanoto', :relation_value => 'mi', :expected_value => true},
+                   {:key => 'mizunoe', :relation_value => 'tori', :expected_value => true},
+                   {:key => 'mizunoto', :relation_value => 'saru', :expected_value => true}]
+
+  test_patterns.each do |pattern|
+    it_behaves_like :gakushi_check_validation, pattern
+    it_behaves_like :gakushi_get_validation, pattern
+  end
+end
+
+describe Jikkan, "天財" do
+  shared_examples_for :tenzai_check_validation do |params|
+    subject{Jikkan.by_key(key).tenzai?(relation)}
+    it_behaves_like :relation_check_validation_jikkan, params
+  end
+
+  shared_examples_for :tenzai_get_validation do |params|
+    subject{Jikkan.by_key(key).tenzai}
+    it_behaves_like :relation_get_validation_jikkan, params
+  end
+
+  test_patterns = [{:key => 'kinoe', :relation_value => 'tsuchinoe', :expected_value => true},
+                   {:key => 'kinoto', :relation_value => 'tsuchinoto', :expected_value => true},
+                   {:key => 'hinoe', :relation_value => 'kanoe', :expected_value => true},
+                   {:key => 'hinoto', :relation_value => 'kanoto', :expected_value => true},
+                   {:key => 'tsuchinoe', :relation_value => 'mizunoe', :expected_value => true},
+                   {:key => 'tsuchinoto', :relation_value => 'mizunoto', :expected_value => true},
+                   {:key => 'kanoe', :relation_value => 'kinoe', :expected_value => true},
+                   {:key => 'kanoto', :relation_value => 'kinoto', :expected_value => true},
+                   {:key => 'mizunoe', :relation_value => 'hinoe', :expected_value => true},
+                   {:key => 'mizunoto', :relation_value => 'hinoto', :expected_value => true}]
+
+  test_patterns.each do |pattern|
+    it_behaves_like :tenzai_check_validation, pattern
+    it_behaves_like :tenzai_get_validation, pattern
+  end
+end
